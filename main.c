e13542835be4b88f0f39656c0cbbbe90da615c88
eim5178 <eim5178@psu.edu>
2020-09-16 18:04:16 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+//Author: Evelyn Moore eim5178@psu.edu
+//Collaborators:
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int digit_sum(int n) {
+  if (n<10){
+    return n;
+  }
+  else{
+    double l = n%10;
+    double d = n/10;
+    n = l+digit_sum(d);
+    return n;
+  }
+}
+int main(void) {
+  int n = atof(readline("Enter an int: "));
+  int n_sum = digit_sum(n);
+  printf("sum of digits of %d is %d.",n,n_sum);
+  return 0;
+}
+
